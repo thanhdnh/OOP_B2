b3f73f5c65403feff46d1f7293a2c0fe896285fc
thanhdnh <myhoangthanh@yahoo.com>
2024-01-10 22:10:56 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        SinhVien sv = new SinhVien("SV001", "Nguyen A", 20);
+        //sv.setID("SV001");
+        //sv.setHoTen("Nguyen A");
+        //sv.setTuoi(20);
+        Console.WriteLine($"Sinh vien(ID=>{sv.getID()}, HoTen=>{sv.getHoTen()}, Tuoi=>{sv.getTuoi()})");
+        SinhVien sv2 = new SinhVien(sv);
+        sv2.setHoTen("Le B");
+        Console.WriteLine($"Sinh vien(ID=>{sv.getID()}, HoTen=>{sv.getHoTen()}, Tuoi=>{sv.getTuoi()})");
+        Console.ReadLine();
+    }
+}
---
+++ SinhVien.cs
@@ -0,0 +1,26 @@
+public class SinhVien{
+    //Khai báo fields
+    private string id;
+    private string hoten;
+    private byte tuoi;
+    //Khai báo các Properties
+    public SinhVien(string myid, string myhoten, byte mytuoi){
+        id = myid;
+        hoten = myhoten;
+        tuoi = mytuoi;
+    }
+    public SinhVien(SinhVien c){
+        id = c.id;
+        hoten = c.hoten;
+        tuoi = c.tuoi;
+    }
+    public void setID(string myid){id = myid;}
+    public string getID(){return id;}
+    public void setHoTen(string myhoten){hoten=myhoten;}
+    public string getHoTen(){return hoten;}
+    public void setTuoi(byte mytuoi){tuoi=mytuoi;}
+    public byte getTuoi(){return tuoi;}
+
+    //Khai báo các methods
+    //Khai báo các events
+}
